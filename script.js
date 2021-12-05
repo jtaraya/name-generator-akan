d8c2a7830c55ab75f9514e9cea653632ee967d22
jtaraya <jacobtaraya@gmail.com>
2021-12-05 18:41:24 +0300

    added gender function

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function akanName () {
-    const centuryOfBirth = document.getElementById("century-input").value;
-    const yearOfBirth = document.getElementById("year-input").value;
-    const monthOfBirth = document.getElementById("month-input").value;
-    const dateOfBirth = document.getElementById("date-input").value;
-    const gender = document.getElementsByName("gender");
+    var centuryOfBirth = document.getElementById("century-input").value;
+    var yearOfBirth = document.getElementById("year-input").value;
+    var monthOfBirth = document.getElementById("month-input").value;
+    var dateOfBirth = document.getElementById("date-input").value;
+    var gender = document.getElementsByName("gender");
+
+    function takegender () {
+        for (var gender of genders ) {
+            if (gender.click){
+                return gender.value;
+            }
+        }
+    }
 
 }
