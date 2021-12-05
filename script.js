3841f3492e57173c4b26f93c819133e40b0dfcee
jtaraya <jacobtaraya@gmail.com>
2021-12-05 18:34:56 +0300

    adding function to akanName

---
+++ script.js
@@ -0,0 +1,8 @@
+function akanName () {
+    const centuryOfBirth = document.getElementById("century-input").value;
+    const yearOfBirth = document.getElementById("year-input").value;
+    const monthOfBirth = document.getElementById("month-input").value;
+    const dateOfBirth = document.getElementById("date-input").value;
+    const gender = document.getElementsByName("gender");
+
+}
